0a570e207cadf5ef8fa8c9fab9d9d523c0535794
AlbertoIJS <aijs.cdmx@gmail.com>
2024-10-11 14:41:03 -0600

    Move to vite.js with react and react router

--- Practice2/Practice2-frontend/signup.js
+++
@@ -1,9 +0,0 @@
-const form = document.getElementById("signup")
-
-form.addEventListener("submit", async (e) => {
-    e.preventDefault()
-
-    const {email, password} = e.target.elements
-
-    console.log({email, password})
-})
