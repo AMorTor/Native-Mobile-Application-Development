9caac97ac25692389d34c96b9e7b9e510b2b5849
AlbertoIJS <aijs.cdmx@gmail.com>
2024-10-12 23:30:21 -0600

    Add user page to create new users

---
+++ Practice2/Practice2-frontend/src/pages/NewUser/NewUser.jsx
@@ -0,0 +1,25 @@
+import { Form } from "react-router-dom";
+import Layout from "../../components/Layout/index.js";
+import UserForm from "../../components/UserForm";
+import Button from "../../components/Button/index.js";
+
+export default function NewUser() {
+  return (
+    <Layout>
+      <section className="flex gap-6 flex-col md:flex-row justify-between md:items-center">
+        <div>
+          <h1 className="font-semibold text-gray-900">Nuevo perfil</h1>
+          <p className="mt-2 text-sm text-gray-700">
+            Aquí puedes crear un nuevo perfil de usuario con la información que
+            desees
+          </p>
+        </div>
+      </section>
+      <Form action="/user" method="POST" className="mt-10">
+        <UserForm>
+          <Button>Guardar</Button>
+        </UserForm>
+      </Form>
+    </Layout>
+  );
+}
--- Practice2/Practice2-frontend/src/pages/router.jsx
+++ Practice2/Practice2-frontend/src/pages/router.jsx
@@ -5,12 +5,19 @@ import Home from "./Home";
 import { homeLoader } from "./Home/Home.handlers.js";
 import User from "./User/index.js";
 import { userAction, userLoader } from "./User/User.handlers.js";
+import NewUser from "./NewUser/index.js";
+import { newUserAction } from "./NewUser/NewUser.handlers.js";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
     loader: homeLoader,
+  },
+  {
+    path: "/user",
+    element: <NewUser />,
+    action: newUserAction,
   },
   {
     path: "/user/:id",
